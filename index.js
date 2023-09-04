2dee7dedc6eb8bd409bcd170a7e9546a3b7536af
greatdane <vanderbyljeandre@gmail.com>
2023-09-04 15:18:00 +0200

    add info

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ let myCourses = ["Learn CSS Animations", "UI Design Fundamentals", "Intro to Cle
 // Create a function that takes a single parameter, an array,
 // and logs all the items of the array to the console.
 // Call the function while passing in myCourses as an argument
+
+function toDay (keep){
+    for (let i = 0; i < keep.length; i++){
+        console.log(keep[i])
+    }
+
+}
+
+toDay(myCourses)
+
+// We need always call the functin with the array as the method 
+// always need to use a for loop to aquire info on an array 
